beb50c9e0d48eddadc71839d7632ccbded2474aa
Zavier <zav765@gmail.com>
2020-09-26 16:03:08 -0500

    experian processor wip

--- vendors/experian.js
+++ vendors/experian.js
@@ -1,6 +1,32 @@
 const flat = require('flat');
 const unflatten = require('flat').unflatten;
 const helpers = require('../helpers');
+const moment = require('moment');
+
+function coerceValue(options) {
+  let { data_type, value } = options;
+  try {
+    switch (data_type) {
+    case 'String':
+      return (typeof value === 'string') ? value : String(value);
+    case 'Number': 
+      return (value && !isNaN(Number(value))) ? Number(value) : value;
+    case 'Boolean':
+      if (typeof value === 'string' && value.toLowerCase() === 'true') {
+        value = true;
+      } else if (typeof value === 'string' && value.toLowerCase() === 'false') {
+        value = false;
+      }
+      return value;  
+    case 'Date':
+      return (value && moment(value).format('MM/DD/YYYY') !== 'Invalid date') ? moment(value).format('MM/DD/YYYY') : value;
+    default:
+      return value;  
+    }
+  } catch (e) {
+    return value;
+  }
+}
 
 function processExperianCreditReport(options) {
     let { dataintegration, segment, api_response, responseTraversalPath, } = options;
@@ -27,7 +53,7 @@ function processExperianCreditReport(options) {
         
         let variable = output_variable.title;
         
-        if (variable) acc[ variable ] = (value !== null && value !== undefined) ? helpers.coerceValue({ data_type: curr.data_type, value, }) : null;
+        if (variable) acc[ variable ] = (value !== null && value !== undefined) ? coerceValue({ data_type: curr.data_type, value, }) : null;
         return acc;
       } catch (err) {
         return acc;
